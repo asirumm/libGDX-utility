4e3c45e3a1503396058f2d8913a7e95fb15ec99d
haslam <haslamhdz71@gmail.com>
2025-07-24 09:15:22 +0700

    update ; log

--- core/src/main/java/com/diagantika/Main.java
+++ core/src/main/java/com/diagantika/Main.java
@@ -1,12 +1,7 @@
 package com.diagantika;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.diagantika.Screen.SplashScreen;
 import com.diagantika.ScreenManager.ApplicationScreen;
 import com.diagantika.ScreenManager.Transition.AbstractScreenTransition;
@@ -14,15 +9,12 @@ import com.diagantika.ScreenManager.Transition.CircleTransition;
 import com.diagantika.ScreenManager.Transition.FadeTransition;
 import com.diagantika.Util.Bean;
 import com.diagantika.Util.Logger;
-import com.diagantika.Util.LoggerConfig;
-
 import java.util.HashMap;
 
 public class Main extends ApplicationScreen {
     public SpriteBatch batch;
-    private LoggerConfig loggerConfig;
-    public Logger<Main> log = new Logger<>(Main.class, Bean.getLogConfigInstance()) ;
-    private HashMap<TRANSITION, AbstractScreenTransition> transitions;
+    public Logger<Main> log ;
+    public HashMap<TRANSITION, AbstractScreenTransition> transitions;
 
     public enum TRANSITION{
         FADE_TRANSITION,
@@ -31,10 +23,10 @@ public class Main extends ApplicationScreen {
 
     @Override
     public void create() {
-        batch = super.batch;
-        loggerConfig = new LoggerConfig();
+        super.create();
 
-        loggerConfig.configure(false, LoggerConfig.LogLevel.DEBUG);
+        log =  new Logger<>(Main.class, Bean.getLogConfigInstance());
+        batch = super.batch;
 
         log.info("memulai setup");
 
@@ -44,10 +36,6 @@ public class Main extends ApplicationScreen {
 
         setScreen(new SplashScreen(),transitions.get(TRANSITION.CIRCLE_TRANSITION));
 
-    }
-
-    public HashMap<TRANSITION, AbstractScreenTransition> getTransitions() {
-        return transitions;
     }
 
     @Override
